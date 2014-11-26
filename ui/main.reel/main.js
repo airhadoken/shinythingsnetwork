755c2b1280d68db5f309830ecb29324f5c76c5e4
Bradley Momberger <noreply>
2014-11-26 23:35:53 +0000

    Update components

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -14,4 +14,24 @@ exports.Main = Component.specialize(/** @lends Main# */ {
             this.super();
         }
     }
+});  
+  
+exports.RangeController = Component.specialize({
+    constructor: {
+    value: function RangeController() {
+    this.content = [{
+    "quote": "If music be the food of love, play on.",
+    "important": false
+}, {
+    "quote": "O Romeo, Romeo! wherefore art thou Romeo?",
+    "important": true
+}, {
+    "quote": "All that glitters is not gold.",
+    "important": false
+}, {
+    "quote": "I am amazed and know not what to say.",
+    "important": false
+}];
+                                    }
+                                    },
 });
